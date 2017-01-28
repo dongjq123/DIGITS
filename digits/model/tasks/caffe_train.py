38073d8761075d742687a14ecccd5a6e4e997263
Jed <jed-frey@users.noreply.github.com>
2017-01-28 16:27:18 -0500

    Git Auto Commit: Sat Jan 28 16:27:18 EST 2017

--- digits/model/tasks/caffe_train.py
+++ digits/model/tasks/caffe_train.py
@@ -10,6 +10,8 @@ import re
 import sys
 import time
 
+from functools import reduce
+
 from google.protobuf import text_format
 import numpy as np
 import platform
@@ -1128,7 +1130,7 @@ class CaffeTrainTask(TrainTask):
         if os.path.exists(self.path(self.CAFFE_LOG)):
             output = tail(self.path(self.CAFFE_LOG), 40)
             lines = []
-            for line in output.split('\n'):
+            for line in output.decode().split('\n'):
                 # parse caffe header
                 timestamp, level, message = self.preprocess_output_caffe(line)
 
@@ -1405,7 +1407,7 @@ class CaffeTrainTask(TrainTask):
         y, x = np.histogram(data, bins=20)
         y = list(y.astype(np.float32))
         ticks = x[[0, len(x) / 2, -1]]
-        x = [((x[i] + x[i + 1]) / 2.0).astype(np.float32) for i in xrange(len(x) - 1)]
+        x = [((x[i] + x[i + 1]) / 2.0).astype(np.float32) for i in range(len(x) - 1)]
         ticks = list(ticks.astype(np.float32))
         return (mean, std, [y, x, ticks])
 
@@ -1452,7 +1454,7 @@ class CaffeTrainTask(TrainTask):
             data_shape = (constants.DEFAULT_BATCH_SIZE,) + data_shape
 
         outputs = None
-        for chunk in [caffe_images[x:x + data_shape[0]] for x in xrange(0, len(caffe_images), data_shape[0])]:
+        for chunk in [caffe_images[x:x + data_shape[0]] for x in range(0, len(caffe_images), data_shape[0])]:
             new_shape = (len(chunk),) + data_shape[1:]
             if net.blobs['data'].data.shape != new_shape:
                 net.blobs['data'].reshape(*new_shape)
