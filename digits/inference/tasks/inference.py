743956216fd11465b064bc95cba7441ef73096e9
Jed <jed-frey@users.noreply.github.com>
2017-01-28 17:17:35 -0500

    Git Auto Commit: Sat Jan 28 17:17:35 EST 2017

--- digits/inference/tasks/inference.py
+++ digits/inference/tasks/inference.py
@@ -73,11 +73,11 @@ class InferenceTask(Task):
         self.inference_log = open(self.path(self.inference_log_file), 'a')
         if type(self.images) is list:
             # create a file to pass the list of images to perform inference on
-            imglist_handle, self.image_list_path = tempfile.mkstemp(dir=self.job_dir, suffix='.txt')
-            for image_path in self.images:
-                os.write(imglist_handle, "%s\n".encode() % image_path)
-            os.close(imglist_handle)
-
+            _, self.image_list_path = tempfile.mkstemp(dir=self.job_dir, suffix='.txt')
+            with open(self.image_list_path, "w") as imglist_handle:
+                for image_path in self.images:
+                    print(image_path, file=imglist_handle)
+                    
     @override
     def process_output(self, line):
         self.inference_log.write('%s\n' % line)
